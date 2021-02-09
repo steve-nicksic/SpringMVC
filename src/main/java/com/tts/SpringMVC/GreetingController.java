e2c386a8db89c43deb6f902416809137f61e87a6
steve-nicksic <65583459+steve-nicksic@users.noreply.github.com>
2021-02-08 22:05:17 -0600

    Created Greeting Controller file with @Controller annotation and @Getmapping annotation for /greeting

---
+++ src/main/java/com/tts/SpringMVC/GreetingController.java
@@ -0,0 +1,18 @@
+package com.tts.SpringMVC;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+//adding @Controller annotation
+@Controller
+public class GreetingController {
+
+    //adding @GetMapping annotation for /greeting
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
