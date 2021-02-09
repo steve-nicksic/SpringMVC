a32ae8188905427b8fbc1b2a97d12efbc1fee6ca
steve-nicksic <65583459+steve-nicksic@users.noreply.github.com>
2021-02-08 23:30:47 -0600

    Added greeting form as Model object to Controller to expose a new greeting to view the template. This includes @PostMapping for /greeting and addition of new Greeting object.

--- src/main/java/com/tts/SpringMVC/GreetingController.java
+++ src/main/java/com/tts/SpringMVC/GreetingController.java
@@ -3,6 +3,8 @@ package com.tts.SpringMVC;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 //adding @Controller annotation
@@ -14,5 +16,33 @@ public class GreetingController {
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
+    }
+
+    @PostMapping("/greeting")
+    public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
+        model.addAttribute("greeting", greeting);
+        return "result";
+    }
+
+    public class Greeting {
+        
+        private long id;
+        private String content;
+
+        public long getId() {
+            return id;
+        }
+
+        public void setId(long id) {
+            this.id = id;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
     }
 }
